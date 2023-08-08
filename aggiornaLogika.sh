b15bb01457450390e92e6b5a095048768a149e8d
d.carlucci <d.carlucci@almaviva.it>
2023-08-08 20:28:55 +0200

    Update aggiornaLogika.sh

--- aggiornaLogika.sh
+++ aggiornaLogika.sh
@@ -3,16 +3,7 @@ node {
         sh '''
 #!/bin/bash
 
-process_name="MainClassLogika"  
-java_pid=$(jps | grep "$process_name" | awk '{print $1}')
-
-if [ -z "$java_pid" ]; then
-    echo "Nessun processo Java trovato con il nome: $process_name"
-else
-    echo "Terminazione del processo Java con PID: $java_pid"
-    kill "$java_pid"
-fi
-/var/lib/jenkins/workspace/logika/go.sh &
+/home/daniele/aggiornaLogika.sh/var/lib/jenkins/workspace/logika/go.sh &
         '''
     }
 }
