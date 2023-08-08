4a8827a9617a558f395252cdff676d172b9a2e51
d.carlucci <d.carlucci@almaviva.it>
2023-08-08 16:32:36 +0200

    Update aggiornaLogika.sh

--- aggiornaLogika.sh
+++ aggiornaLogika.sh
@@ -1,34 +1,19 @@
-// Trova il PID del processo Java utilizzando jps e filtra per il nome del processo
-def processName = "MainClassLogika" // Sostituisci con il nome del tuo processo Java
-def processList = new ProcessBuilder("jps").redirectErrorStream(true).start()
-def reader = new BufferedReader(new InputStreamReader(processList.getInputStream()))
-def javaPid = reader.lines().collect { line ->
-    if (line.contains(processName)) {
-        line.split()[0]
-    }
-}.find()
+// Comando Bash da eseguire
+def bashCommand = "echo 'Hello from Bash'"
 
-if (!javaPid) {
-    println "Nessun processo Java trovato con il nome: $processName"
+// Esecuzione del comando Bash
+def process = bashCommand.execute()
+process.waitFor()
+
+// Lettura dell'output del comando
+def output = process.in.text
+def error = process.err.text
+
+// Elaborazione dell'output
+if (process.exitValue() == 0) {
+    println "Comando Bash eseguito con successo:"
+    println output
 } else {
-    println "Terminazione del processo Java con PID: $javaPid"
-    def processKill = new ProcessBuilder("kill", javaPid).redirectErrorStream(true).start()
-    def killReader = new BufferedReader(new InputStreamReader(processKill.getInputStream()))
-    killReader.lines().forEach { println it }
-    processKill.waitFor()
-}
-
-def gitDir = new File("/home/daniele/githubrepository/logika/")
-if (gitDir.exists()) {
-    def gitPull = new ProcessBuilder("git", "pull").directory(gitDir).redirectErrorStream(true).start()
-    def pullReader = new BufferedReader(new InputStreamReader(gitPull.getInputStream()))
-    pullReader.lines().forEach { println it }
-    gitPull.waitFor()
-
-    def launchScript = new ProcessBuilder("sh", "lancia.sh").directory(gitDir).redirectErrorStream(true).start()
-    def launchReader = new BufferedReader(new InputStreamReader(launchScript.getInputStream()))
-    launchReader.lines().forEach { println it }
-    launchScript.waitFor()
-} else {
-    println "La directory git non esiste: ${/home/daniele/githubrepository/logika/}"
+    println "Errore durante l'esecuzione del comando Bash:"
+    println error
 }
