bfa044c1d69869785e737155f775ba6b23e60553
d.carlucci <d.carlucci@almaviva.it>
2023-08-08 15:48:10 +0200

    Update aggiornaLogika.sh

--- aggiornaLogika.sh
+++ aggiornaLogika.sh
@@ -1,15 +1,34 @@
-#!/bin/bash
+// Trova il PID del processo Java utilizzando jps e filtra per il nome del processo
+def processName = "MainClassLogika" // Sostituisci con il nome del tuo processo Java
+def processList = new ProcessBuilder("jps").redirectErrorStream(true).start()
+def reader = new BufferedReader(new InputStreamReader(processList.getInputStream()))
+def javaPid = reader.lines().collect { line ->
+    if (line.contains(processName)) {
+        line.split()[0]
+    }
+}.find()
 
-# Trova il PID del processo Java utilizzando jps e filtra per il nome del processo
-process_name="MainClassLogika"  # Sostituisci con il nome del tuo processo Java
-java_pid=$(jps | grep "$process_name" | awk '{print $1}')
+if (!javaPid) {
+    println "Nessun processo Java trovato con il nome: $processName"
+} else {
+    println "Terminazione del processo Java con PID: $javaPid"
+    def processKill = new ProcessBuilder("kill", javaPid).redirectErrorStream(true).start()
+    def killReader = new BufferedReader(new InputStreamReader(processKill.getInputStream()))
+    killReader.lines().forEach { println it }
+    processKill.waitFor()
+}
 
-if [ -z "$java_pid" ]; then
-    echo "Nessun processo Java trovato con il nome: $process_name"
-else
-    echo "Terminazione del processo Java con PID: $java_pid"
-    kill "$java_pid"
-fi
-cd githubrepository/logika/
-git pull
-sh lancia.sh
+def gitDir = new File("githubrepository/logika/")
+if (gitDir.exists()) {
+    def gitPull = new ProcessBuilder("git", "pull").directory(gitDir).redirectErrorStream(true).start()
+    def pullReader = new BufferedReader(new InputStreamReader(gitPull.getInputStream()))
+    pullReader.lines().forEach { println it }
+    gitPull.waitFor()
+
+    def launchScript = new ProcessBuilder("sh", "lancia.sh").directory(gitDir).redirectErrorStream(true).start()
+    def launchReader = new BufferedReader(new InputStreamReader(launchScript.getInputStream()))
+    launchReader.lines().forEach { println it }
+    launchScript.waitFor()
+} else {
+    println "La directory git non esiste: ${gitDir.absolutePath}"
+}
