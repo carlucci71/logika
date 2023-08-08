faf920c89033ef36d0a29f97d48cb6d84e1c1da5
d.carlucci <d.carlucci@almaviva.it>
2023-08-08 17:59:11 +0200

    Update aggiornaLogika.sh

--- aggiornaLogika.sh
+++ aggiornaLogika.sh
@@ -1,11 +1,18 @@
 node {
     stage('Stop and Restart JAR') {
         sh '''
-        #!/bin/bash
-		process_name="MainClassLogika"
-		java_pid=$(jps | grep "$process_name" | awk '{print $1}')
-        kill "$java_pid"
-        mvn spring-boot:run &
+#!/bin/bash
+
+process_name="MainClassLogika"  
+java_pid=$(jps | grep "$process_name" | awk '{print $1}')
+
+if [ -z "$java_pid" ]; then
+    echo "Nessun processo Java trovato con il nome: $process_name"
+else
+    echo "Terminazione del processo Java con PID: $java_pid"
+    kill "$java_pid"
+fi
+mvn spring-boot:run -f /home/daniele/githubrepository/logika/pom.xml &
         '''
     }
 }
